2cdfd75ecaf580da9c8e5c499638b7909f606cad
Xinyang <xinyzhang9@gmail.com>
2016-11-09 15:46:59 -0800

    c

--- myMap.js
+++ myMap.js
@@ -19,8 +19,15 @@ var map_items = [
 //2. create pokemon image on map
 function get_pokemon_layer_from_map_items(map_items){
 	var layer = new Microsoft.Maps.Layer();
-	var pushpins = Microsoft.Maps.TestDataGenerator.getPushpins(10, map.getBounds());
+	var pushpins = [];
+	for(var i in map_items){
+		var map_item = map_items[i];
+		var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item['latitude'],map_item['longitude']), 
+			{ icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png'});
+		pushpins.push(pushpin);
+	}
 	layer.add(pushpins);
+
 	return layer;
 }
 
