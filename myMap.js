41590b98c0beb113a5aeb7ba2c84aeb8f72e6a9f
Xinyang <xinyzhang9@gmail.com>
2016-11-09 16:03:19 -0800

    time

--- myMap.js
+++ myMap.js
@@ -10,7 +10,7 @@ function loadMapScenario() {
 var map_items = [
 	{
 		"pokemon_id": 12,
-		"expire": 1234567,
+		"expire": 1478739751,
 		"longitude": -121.9872018,
 		"latitude": 37.391752
 	}
@@ -24,7 +24,7 @@ function get_pokemon_layer_from_map_items(map_items){
 		var map_item = map_items[i];
 		var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item['latitude'],map_item['longitude']), 
 			{ icon: 'images/pushpin_images/pokemon/'+map_item['pokemon_id']+'.png',
-				title: 'expire_time'
+				title: get_counter_down_time_from_expire_epoch(map_item['expire'])
 			});
 		pushpins.push(pushpin);
 	}
@@ -38,6 +38,13 @@ function add_pokemon_layer(){
 	map.layers.insert(pokemon_layer);
 }
 
+function get_counter_down_time_from_expire_epoch(epoch){
+	var now_time = new Date().getTime() / 1000;
+	var time_left = epoch - now_time;
+	var second = Math.floor(time_left % 60);
+	var minute = Math.floor(time_left / 60);
+	return minute +' : '+second;
+}
 
 
 //3. add pokemon counter down refresh
